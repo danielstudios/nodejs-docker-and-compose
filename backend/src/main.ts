5ff76cffc89214c0e692055c5835d4611fd48de1
Daniel <danielvonyavkin@gmail.com>
2025-07-17 13:58:59 +0300

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors({
+    origin: [
+      'https://danielkupi.students.nomorepartiessbs.ru',
+      'https://api.danielkupi.students.nomorepartiessbs.ru'
+    ],
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
+    credentials: true,
+    maxAge: 86400,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
