e7ab648d48bd317733e56884ebaa38c81f73e7c1
Daniel <danielvonyavkin@gmail.com>
2025-07-17 14:50:03 +0300

    add new cookies setting

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -23,6 +23,9 @@ export class AuthController {
     const token = await this.authService.signin(signinUserDto);
     response.cookie('jwt', token, {
       httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      domain: '.danielkupi.students.nomorepartiessbs.ru',
       maxAge: 36000000,
     });
 
@@ -38,6 +41,9 @@ export class AuthController {
     const token = this.authService.auth(createdUser);
     response.cookie('jwt', token, {
       httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      domain: '.danielkupi.students.nomorepartiessbs.ru',
       maxAge: 36000000,
     });
 
